5d8057d2a3be74a8e38f4651cb1d28587b8605b0
Ben <ringerben1@gmail.com>
2022-04-13 16:59:31 -0400

    first push

---
+++ app.js
@@ -0,0 +1,47 @@
+var a = 10;
+var b = 20;
+var c = 30;
+var d = 40;
+var e = 50;
+var f = 60;
+var g = 70;
+var h = 80;
+var i = 90;
+var j = 100;
+
+
+// Addition
+console.log(a + b);
+console.log(c + d);
+
+// Subtraction
+console.log(i - a);
+console.log(j - e);
+
+// Multiplication
+console.log(b * c);
+console.log(e * f);
+
+
+// Division
+console.log(j / a);
+console.log(d / b);
+
+// Increment
+console.log(a++);
+
+
+myArray = [];
+for(var n = 10; n <= 20; n++){
+    console.log(n);
+    myArray.push(n);
+}
+
+// Decrement
+console.log(b--);
+
+myArray = [];
+for(var n = 10; n >= 0; n--){
+    console.log(n);
+    myArray.push(n);
+}
